e55684e6f8bfcf8c5e3d198e44da4f464d3d384e
hiba <hibzrashed@gmail.com>
2022-05-25 13:21:36 -0500

    commit tree 798b0e25d3cea827f64480e581ee5dfa66e57add

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+//document.addEventListener("DOMContentLoaded", function() {
+    //console.log("The DOM has loaded");
+  //});
+
+//document.addEventListener('DOMContentLoaded',function(){
+
+    document.addEventListener("DOMContentLoaded", function() {
+        console.log("The DOM has loaded");
+
+        document.querySelector('p').textContent = "This is really cool!"
+    //let text = document.getElementId('text').textContent("This is really cool")
+    
+    
+      });
+      //document.querySelector('This is really cool!')
+
+    //document.querySelector('p'))
